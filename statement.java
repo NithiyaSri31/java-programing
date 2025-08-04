d2e9ed29b47c9d6ab989f46b20acd70c00cc3ff7
NithiyaSri31 <nithiyasri9186@mountzion.ac.in>
2025-08-04 12:26:35 +0530

    Create statement.java

---
+++ statement.java
@@ -0,0 +1,15 @@
+public class name{
+    public static void main(String[] args){
+     int anu= 5;
+       int banu= 20;
+          if (anu>banu)
+          {
+      
+          System.out.println("anu is major");
+          }
+          else {
+         System.out.println("banu is major");
+          }
+        
+    }
+}
